1f44ae7a92bf5223c78e29e9c1290462330265e8
Deneb331 <juanjo1.seriousmail@gmail.com>
2022-09-03 13:53:49 +0000

    Add code for division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,7 +126,11 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 < operand2 ? operand1 : operand2;
+    let op1 = operand1 > operand2 ? operand1 : operand2;
+    let op2 = operand1 < operand2 ? operand1 : operand2;
+    let roundedResult = Math.ceil(op1 / op2);
+    op1 = op2 * roundedResult;
+    document.getElementById('operand1').textContent = op1;
+    document.getElementById('operand2').textContent = op2;
     document.getElementById('operator').textContent = "/";
 }
